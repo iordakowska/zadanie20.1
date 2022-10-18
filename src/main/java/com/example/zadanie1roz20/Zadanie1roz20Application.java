d1950526a35cb65a3abbef37349e2bdbb44178f2
i.ordakowska <i.ordakowska@gmail.com>
2022-10-18 13:33:49 +0200

    Stworzenie projektu

---
+++ src/main/java/com/example/zadanie1roz20/Zadanie1roz20Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie1roz20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie1roz20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie1roz20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie1roz20/Zadanie1roz20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie1roz20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie1roz20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
